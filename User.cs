de6c6afc08ec91738a365ab22d22806b69da2698
FiercelyPierce <cir21001@byui.edu>
2023-03-03 17:30:45 -0700

    Add project files.

---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Balance { get; set; }
+
+        public User() 
+        {
+            Id = 0;
+            Name = string.Empty;
+            Balance = string.Empty;
+
+        }
+
+    }
+}
